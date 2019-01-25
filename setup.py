6b029c028d23b6bffb3e0ed0729ac08564fc1581
JC <juancarlos@sevilla.es>
2019-01-25 01:11:50 -0800

    Improved uploading script

--- setup.py
+++ setup.py
@@ -1,25 +1,20 @@
 from setuptools import find_packages, setup
 
-with open('README.md', 'r') as fh:
-	long_description = fh.read()
+with open('README.md', 'r', encoding='utf-8') as f:
+	long_description = f.read()
+
+with open('requirements.txt', 'r', encoding='utf-8') as f:
+	requirements = f.read().strip().split()
 
 setup(
 	name='tablextract',
-	version='1.0.10',
+	version='1.0.11',
 	author='Juan C. Roldán',
 	author_email='juancarlos@sevilla.es',
 	description='Extract the information represented in any HTML table',
 	long_description=long_description,
 	long_description_content_type='text/markdown',
-	install_requires=[
-		'bs4>=0.0.1',
-		'etk>=2.1.6',
-		'nltk>=3.3',
-		'requests>=2.18.4',
-		'scikit-learn>=0.19.2',
-		'wikipedia-api>=0.3.7',
-		'selenium>=3.14.1'
-	],
+	install_requires=requirements,
 	url='https://github.com/juancroldan/tablextract',
 	packages=find_packages(),
 	package_data={'': ['resources/add_render.js']},
--- upload.bat
+++ upload.bat
@@ -5,5 +5,8 @@ rmdir /Q /S tablextract.egg-info
 rmdir /Q /S tablextract-*
 echo Please, open setup.py and update the version.
 pause
+git add .
+git commit
+git push origin master
 python setup.py sdist bdist_wheel
 python -m twine upload dist/*
